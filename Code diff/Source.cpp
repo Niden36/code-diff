fc6c377d987f00b8bbcc5458d6fd68ffe0e30ab9
niden36 <niden36@gmail.com>
2016-09-04 07:32:32 +0200

    almost done

--- Code diff/Source.cpp
+++ Code diff/Source.cpp
@@ -6,16 +6,18 @@
 #include <string.h>
 #include "blocks.h"
 #include "compare.h"
-
+#include "clearbuffs.h"
+#include "stringenlarge.h"
+#include "clearscrn.h"
 using namespace std;
 
 
 int main()
 {
-	string str1, str2, buff1, buff2, word, stringtoinsert;
+	string str1, str2, buff1, buff2,buff3, word, stringtoinsert;
 	char c;
-
-	int wordcounter;
+	int mark;
+	int wordcounter,wordcountercheck;
 	int numberofblocks;
 	Blocks X[10];
 	int blockselection;
@@ -57,6 +59,8 @@ int main()
 	if (c == 89 || c == 121)
 	{
 		blockselection = 1; //<<<<<<<<<
+		wordcounter = 1;  //<<<<<<<<<<
+		word = "Three"; // <<<<<<<<<<<
 		cout << "which block would you like to add" << endl;;
 		//cin >> blockselection;
 		cout << "write the word after which you would like to place it" << endl;;
@@ -64,9 +68,39 @@ int main()
 		cout << "if the word appears in the file once type in '1' if it appears several times then type in after which reappearance of the word you would like to put your block in" << endl;
 		//cin >> wordcounter;
 		stringtoinsert = X[blockselection - 1].str;
+		int cc = 0;
+		wordcountercheck = 0;
+		//stringenlarge(word);
+
+	    while ( cc  <= len1)
+		{
+			while (str1[cc] !=' ' && str1[cc] != '\0')
+			{
+				stringenlarge(buff3);
+				buff3[cc] = str1[cc];
+ 				cc++;
+
+
+			}
+			if(buff3==word)
+			{
+				wordcountercheck++;
+				if (wordcounter == wordcountercheck)
+				{
+					mark = cc;
+					break;
+				}
+				
+			}
+			clearbuff(buff3);
+			c++;
+		}
+		
 
 	}
+	word.insert(0, " ");
+	str1.insert(mark, word);
 	cout << str1;
-	cout << endl << stringtoinsert << endl;
+
 	return 0;
 }
